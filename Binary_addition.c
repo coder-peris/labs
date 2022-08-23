8c097e53c85f4f847728c04ff019ab1131ab3b5a
PERIS <periskc@gmail.com>
2022-08-23 15:02:01 +0545

    labs

--- Binary_addition.c
+++ Binary_addition.c
@@ -23,3 +23,23 @@ int main()
         printf("%d", sum[i]);
     system("pause>0");
 }
+
+/* Alternate and shortest in world: */
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n1, n2, i, carry = 0, temp, sum = 0;
+    printf("Enter two binary numbers:\n");
+    scanf("%d %d", &n1, &n2);
+    for (i = 0; n1 != 0 || n2 != 0; i++)
+    {
+        temp = (n1 % 10 + n2 % 10 + carry) % 2;
+        carry = (n1 % 10 + n2 % 10 + carry) / 2;
+        n1 /= 10;
+        n2 /= 10;
+        sum += temp * pow(10, i);
+    }
+    sum += carry * pow(10, i);
+    printf("The sum of two binary numbers is : %d", sum);
+}
